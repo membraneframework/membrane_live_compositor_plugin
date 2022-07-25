b2ae0035bf7afa46b16ba990bec2540987b2f1cb
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-07-25 11:28:12 +0200

    Apply requested changes

--- c_src/ffmpeg_video_compositor/video_compositor.spec.exs
+++ c_src/ffmpeg_video_compositor/video_compositor.spec.exs
@@ -9,3 +9,5 @@ spec create(
      ) :: {:ok :: label, state} | {:error :: label, reason :: atom}
 
 spec apply_filter(left_payload :: payload, right_payload :: payload, state) :: {:ok :: label, payload} | {:error :: label, reason :: atom}
+
+dirty :cpu, apply_filter: 3
