9dab788a207ec25bf7d0ba738b8aad52cc548aba
WojciechBarczynski <wojciech.barczynski@swmansion.com>
2022-07-26 12:05:52 +0200

    simplify options passing

--- lib/start_video_demos.exs
+++ lib/start_video_demos.exs
@@ -8,17 +8,15 @@ video_framerate = 30
 # implementation is one of: :ffmpeg, :opengl, :nx
 implementation = :nx
 
-options = [
-  %{
-    first_raw_video_path: first_raw_video_path,
-    second_raw_video_path: second_raw_video_path,
-    output_path: output_path,
-    video_width: video_width,
-    video_height: video_height,
-    video_framerate: video_framerate,
-    implementation: implementation
-  }
-]
+options = %{
+  first_raw_video_path: first_raw_video_path,
+  second_raw_video_path: second_raw_video_path,
+  output_path: output_path,
+  video_width: video_width,
+  video_height: video_height,
+  video_framerate: video_framerate,
+  implementation: implementation
+}
 
 {:ok, pid} = Membrane.VideoCompositor.Pipeline.start(options)
 
