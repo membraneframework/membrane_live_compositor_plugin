54010803c3d3a43b686ab0d8436b49714bea5177
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-07-27 14:32:05 +0200

    automate reference videos generation

--- test/support/video_compositor_helpers.ex
+++ test/support/video_compositor_helpers.ex
@@ -4,6 +4,46 @@ defmodule Membrane.VideoCompositor.Test.Utility do
 
   require Membrane.Logger
 
+  @doc """
+  Generate and save a video in the given filename.
+  """
+  @spec generate_testing_raw_video(binary(), Membrane.RawVideo, integer()) :: nil | :ok
+  def generate_testing_raw_video(file_name, video_description, duration) do
+    # ffmpeg -f lavfi -i testsrc=duration=4:size=1280x720:rate=30,format=yuv420p -f rawvideo test/fixtures/4s_30fps.raw
+    video_description_str = """
+    testsrc=\
+    duration=#{duration}\
+    :size=#{video_description.width}x#{video_description.height}\
+    :rate=#{video_description.framerate}\
+    ,format=yuv420p\
+    """
+
+    {result, exit_status} =
+      System.cmd(
+        "ffmpeg",
+        [
+          # does not override the output file if it already exists
+          "-y",
+          # virtual input
+          "-f",
+          "lavfi",
+          # video parameters
+          "-i",
+          video_description_str,
+          # save as raw video
+          "-f",
+          "rawvideo",
+          # output file name
+          file_name
+        ],
+        stderr_to_stdout: true
+      )
+
+    if exit_status != 0 do
+      raise inspect(result)
+    end
+  end
+
   @spec prepare_paths(binary(), binary(), binary()) :: {binary(), binary(), binary()}
   def prepare_paths(input_file_name, ref_file_name, tmp_dir) do
     in_path = "../fixtures/#{input_file_name}" |> Path.expand(__DIR__)
--- test/video_compositor/video_compositor_test.exs
+++ test/video_compositor/video_compositor_test.exs
@@ -3,29 +3,39 @@ defmodule Membrane.VideoCompositor.Test do
 
   import Membrane.Testing.Assertions
 
+  alias Membrane.RawVideo
   alias Membrane.Testing.Pipeline, as: TestPipeline
   alias Membrane.VideoCompositor.Test.Utility
 
   @tag :tmp_dir
-  test "xxx", %{tmp_dir: tmp_dir} do
-    video_width = 1280
-    video_height = 720
-    video_framerate = 30
+  test "compose two raw videos using a pipeline", %{tmp_dir: tmp_dir} do
+    video = %RawVideo{
+      width: 1280,
+      height: 720,
+      framerate: 30,
+      pixel_format: :I420,
+      aligned: nil
+    }
+
     implementation = :ffmpeg
+    duration = 3
 
-    {in_path, out_path, _ref_path} =
-      Utility.prepare_paths("2s_30fps.raw", "ref-all.h264", tmp_dir)
+    {in_path, out_path, ref_path} =
+      Utility.prepare_paths(
+        "#{video.framerate}s_#{video.framerate}fps.raw",
+        "ref-all.h264",
+        tmp_dir
+      )
 
-    IO.inspect(in_path)
-    IO.inspect(out_path)
+    Utility.generate_testing_raw_video(in_path, video, duration)
 
     options = %{
       first_raw_video_path: in_path,
       second_raw_video_path: in_path,
       output_path: out_path,
-      video_width: video_width,
-      video_height: video_height,
-      video_framerate: video_framerate,
+      video_width: video.width,
+      video_height: video.height,
+      video_framerate: video.framerate,
       implementation: implementation
     }
 
@@ -42,12 +52,12 @@ defmodule Membrane.VideoCompositor.Test do
     assert_end_of_stream(pid, :file_sink, :input, 7000)
     TestPipeline.terminate(pid, blocking?: true)
 
-    # Helpers.create_ffmpeg_reference(
-    #   in_path,
-    #   ref_path,
-    #   "drawtext=text='My text':fontcolor=white:box=1:boxcolor=orange:borderw=1:bordercolor=red:fontsize=35:x=(w-text_w)/2:y=w/100"
-    # )
+    Utility.create_ffmpeg_reference(
+      in_path,
+      ref_path,
+      "split[b], pad=iw:ih*2[src], [src][b]overlay=0:h"
+    )
 
-    # Helpers.compare_contents(out_path, ref_path)
+    Utility.compare_contents(out_path, ref_path)
   end
 end
