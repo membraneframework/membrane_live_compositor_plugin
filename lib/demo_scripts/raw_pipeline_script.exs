6f5d19f7fde254e230c548a4bc112edc6ab318fb
WojciechBarczynski <wojciech.barczynski@swmansion.com>
2022-07-29 17:15:17 +0200

    add tests

--- lib/demo_scripts/raw_pipeline_script.exs
+++ lib/demo_scripts/raw_pipeline_script.exs
@@ -1,16 +1,16 @@
 alias Membrane.RawVideo
 
 paths = %{
-  first_raw_video_path: "~/membrane_video_compositor/input_30s_720p.raw",
-  second_raw_video_path: "~/membrane_video_compositor/input_30s_720p.raw",
-  output_path: "~/membrane_video_compositor/output_30s_720p.raw"
+  first_raw_video_path: "~/membrane_video_compositor/input_60s_1080p.raw",
+  second_raw_video_path: "~/membrane_video_compositor/input_60s_1080p.raw",
+  output_path: "~/membrane_video_compositor/output_60s_1920x2160.raw"
 }
 
 caps = %RawVideo{
   aligned: true,
   framerate: {30, 1},
-  width: 1280,
-  height: 720,
+  width: 1920,
+  height: 1080,
   pixel_format: :I420
 }
 
@@ -22,7 +22,6 @@ options = %{
   implementation: implementation
 }
 
-{:ok, pid} = Membrane.VideoCompositor.PipelineH264.start(options)
+{:ok, pid} = Membrane.VideoCompositor.PipelineRaw.start(options)
 
-# will be removed with implementation of Membrane Beamchmark
 Process.sleep(1_000_000)
