c91174126312ebea71a37d74dd1d6bb5d7a9b1d2
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-08-01 11:53:32 +0200

    add ffmeg test

--- test/video_compositor/video_compositor_test.exs
+++ test/video_compositor/video_compositor_test.exs
@@ -4,52 +4,49 @@ defmodule Membrane.VideoCompositor.Test do
   import Membrane.Testing.Assertions
 
   alias Membrane.RawVideo
-  alias Membrane.Testing.Pipeline, as: TestPipeline
+  alias Membrane.Testing.Pipeline, as: TestingPipeline
   alias Membrane.VideoCompositor.Test.Utility
 
   @tag :tmp_dir
-  test "compose two raw videos using a pipeline", %{tmp_dir: tmp_dir} do
-    video = %RawVideo{
+  test "compose two raw videos using a ffmpeg in the pipeline", %{tmp_dir: tmp_dir} do
+    video_caps = %RawVideo{
       width: 1280,
       height: 720,
-      framerate: 30,
+      framerate: {30, 1},
       pixel_format: :I420,
       aligned: nil
     }
 
     implementation = :ffmpeg
-    duration = 3
 
     {in_path, out_path, _ref_path} =
       Utility.prepare_paths(
-        "#{duration}s_#{video.framerate}fps.raw",
+        "input_30s_720p.h264",
         "ref-all.h264",
         tmp_dir
       )
 
-    Utility.generate_testing_raw_video(in_path, video, duration)
+    # Utility.generate_testing_raw_video(in_path, video, duration)
 
     options = %{
-      first_raw_video_path: in_path,
-      second_raw_video_path: in_path,
-      output_path: out_path,
-      video_width: video.width,
-      video_height: video.height,
-      video_framerate: video.framerate,
+      paths: %{
+        first_h264_video_path: in_path,
+        second_h264_video_path: in_path,
+        output_path: out_path
+      },
+      caps: video_caps,
       implementation: implementation
     }
 
     assert {:ok, pid} =
-             TestPipeline.start_link(
-               module: Membrane.VideoCompositor.Pipeline,
+             TestingPipeline.start_link(%TestingPipeline.Options{
+               module: Membrane.VideoCompositor.PipelineH264,
                custom_args: options
-             )
-
-    Membrane.VideoCompositor.Pipeline.play(pid)
+             })
 
     assert_pipeline_playback_changed(pid, _, :playing)
 
-    assert_end_of_stream(pid, :file_sink, :input, 7000)
-    TestPipeline.terminate(pid, blocking?: true)
+    assert_end_of_stream(pid, :file_sink, :input, 100_000)
+    TestingPipeline.terminate(pid, blocking?: true)
   end
 end
