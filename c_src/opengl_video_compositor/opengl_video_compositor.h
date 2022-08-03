fc954a243b192ee1f55ef5d9a992c57033c10541
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2022-08-03 14:24:59 +0200

    Remove unnecessary comments

--- c_src/opengl_video_compositor/opengl_video_compositor.h
+++ c_src/opengl_video_compositor/opengl_video_compositor.h
@@ -1,15 +1,11 @@
 #pragma once
 
-// #define GLFW_INCLUDE_NONE
-// #include <GLFW/glfw3.h>
 #include <EGL/egl.h>
 
 #include "Compositor.h"
 
 struct State {
-    // Compositor compositor;
     Compositor *compositor;
-    // GLFWwindow *window;
     EGLDisplay display;
     EGLSurface surface;
     EGLContext context;
