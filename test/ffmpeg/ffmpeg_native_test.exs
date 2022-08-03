df7968ce94882b252c4a315388c17fd0f0e030fd
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-08-03 10:46:43 +0200

    add tests for multiple video inputs

--- test/ffmpeg/ffmpeg_native_test.exs
+++ test/ffmpeg/ffmpeg_native_test.exs
@@ -6,36 +6,21 @@ defmodule VideoCompositor.FFmpeg.Native.Test do
   alias Membrane.VideoCompositor.Test.Utility
 
   @tag :tmp_dir
-  test "compose doubled raw video frame on top of each other", %{tmp_dir: tmp_dir} do
+  test "compose doubled raw video frames on top of each other", %{tmp_dir: tmp_dir} do
     {in_path, out_path, ref_path} = Utility.prepare_paths("1frame.yuv", "ref-native.yuv", tmp_dir)
-    assert {:ok, frame_a} = File.read(in_path)
-    assert {:ok, frame_b} = File.read(in_path)
 
-    videos = [
-      %RawVideo{
-        width: 640,
-        height: 360,
-        pixel_format: :I420,
-        framerate: nil,
-        aligned: nil
-      },
-      %RawVideo{
-        width: 640,
-        height: 360,
-        pixel_format: :I420,
-        framerate: nil,
-        aligned: nil
-      }
-    ]
+    video = %RawVideo{
+      width: 640,
+      height: 360,
+      pixel_format: :I420,
+      framerate: nil,
+      aligned: nil
+    }
 
-    assert {:ok, ref} = Native.init(videos)
-    frames = [frame_a, frame_b]
-    assert {:ok, out_frame} = Native.apply_filter(frames, ref)
-    assert {:ok, file} = File.open(out_path, [:write])
-    on_exit(fn -> File.close(file) end)
+    n_frames = 2
+    assert compose_n_frames(in_path, out_path, video, n_frames)
 
-    IO.binwrite(file, out_frame)
-    reference_input_path = "../fixtures/1frame.h264" |> Path.expand(__DIR__)
+    reference_input_path = String.replace_suffix(in_path, "yuv", "h264")
 
     Utility.create_ffmpeg_reference(
       reference_input_path,
@@ -44,5 +29,36 @@ defmodule VideoCompositor.FFmpeg.Native.Test do
     )
 
     Utility.compare_contents(out_path, ref_path)
+  end
+
+  @tag :tmp_dir
+  test "compose multiple raw video frames", %{tmp_dir: tmp_dir} do
+    {in_path, out_path, _ref_path} =
+      Utility.prepare_paths("1frame.yuv", "ref-native.yuv", tmp_dir)
+
+    video = %RawVideo{
+      width: 640,
+      height: 360,
+      pixel_format: :I420,
+      framerate: nil,
+      aligned: nil
+    }
+
+    n_frames = 5
+    assert compose_n_frames(in_path, out_path, video, n_frames)
+  end
+
+  defp compose_n_frames(in_path, out_path, video, n_frames) do
+    assert frame = File.read!(in_path)
+    frames = for _ <- 1..n_frames, do: frame
+    videos = for _ <- 1..n_frames, do: video
+
+    assert {:ok, ref} = Native.init(videos)
+    assert {:ok, out_frame} = Native.apply_filter(frames, ref)
+
+    assert {:ok, file} = File.open(out_path, [:write])
+    on_exit(fn -> File.close(file) end)
+
+    IO.binwrite(file, out_frame)
   end
 end
