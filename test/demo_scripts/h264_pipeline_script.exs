5f4f9eaf4384b97077d756c2def74aeefdfd982f
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-08-08 14:03:01 +0200

    Add H264 demo
    It required custom decoder bin (parser+decoder)

--- test/demo_scripts/h264_pipeline_script.exs
+++ test/demo_scripts/h264_pipeline_script.exs
@@ -1,27 +1,37 @@
 alias Membrane.RawVideo
 
 paths = %{
-  first_video_path: "~/membrane_video_compositor/input_120s_4k.h264",
-  second_video_path: "~/membrane_video_compositor/input_120s_4k.h264",
-  output_path: "~/membrane_video_compositor/output_120s.h264"
+  first_video_path: "./test/fixtures/long_videos/input_60s_1080p.h264",
+  second_video_path: "./test/fixtures/long_videos/input_60s_1080p.h264",
+  output_path: "./test/fixtures/tmp_dir/output_120s.h264"
 }
 
 caps = %RawVideo{
   aligned: true,
   framerate: {30, 1},
-  width: 3840,
-  height: 2160,
+  width: 1920,
+  height: 1080,
   pixel_format: :I420
 }
 
 implementation = :nx
 
+parser = Membrane.VideoCompositor.Demo.H264.InputParser
+
+encoder = Membrane.H264.FFmpeg.Encoder
+
+sink = nil
+# sink = Membrane.SDL.Player
+
 options = %{
   paths: paths,
   caps: caps,
-  implementation: implementation
+  implementation: implementation,
+  decoder: parser,
+  sink: sink,
+  encoder: encoder
 }
 
-{:ok, pid} = Membrane.VideoCompositor.PipelineH264.start(options)
+{:ok, pid} = Membrane.VideoCompositor.Demo.PipelineTemplate.start(options)
 
 Process.sleep(1_000_000)
---
+++ test/support/input_parser_h264.ex
@@ -0,0 +1,30 @@
+defmodule Membrane.VideoCompositor.Demo.H264.InputParser do
+  use Membrane.Bin
+  alias Membrane.RawVideo
+
+  def_input_pad :input,
+    demand_unit: :buffers,
+    demand_mode: :auto,
+    caps: :any
+
+  def_output_pad :output,
+    demand_mode: :auto,
+    caps: {RawVideo, pixel_format: one_of([:I420, :I422]), aligned: true}
+
+  @impl true
+  def handle_init(_opts) do
+    children = %{
+      parser: Membrane.H264.FFmpeg.Parser,
+      decoder: Membrane.H264.FFmpeg.Decoder
+    }
+
+    links = [
+      link_bin_input(:input) |> to(:parser) |> to(:decoder) |> to_bin_output(:output)
+    ]
+
+    spec = %ParentSpec{children: children, links: links}
+    IO.inspect("Siema")
+
+    {{:ok, spec: spec}, %{}}
+  end
+end
