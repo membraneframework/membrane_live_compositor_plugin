bd58e1db9b82c324d4137320dc994f5b54c30b0d
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2022-08-25 15:11:27 +0200

    Implement `Membrane.VideoCompositor.FrameCompositor` for OpenGL.
    Both OpenGL implementations (in Rust and C++) now have a corresponding
    module implementing FrameCompositor using them as a backend.

--- lib/frame_compositor/opengl.ex
+++ lib/frame_compositor/opengl.ex
@@ -16,3 +16,46 @@ defmodule Membrane.VideoCompositor.OpenGL do
     {{:ok, merged_frames_binary}, internal_state}
   end
 end
+
+defmodule Membrane.VideoCompositor.OpenGL.Cpp do
+  @moduledoc """
+  This module implements video composition in OpenGL with a C++ backend
+  """
+
+  @behaviour Membrane.VideoCompositor.FrameCompositor
+
+  alias Membrane.VideoCompositor.OpenGL.Native.Cpp
+
+  @impl true
+  def init(caps) do
+    Cpp.init(caps, caps)
+  end
+
+  @impl true
+  def merge_frames(%{first: first_frame, second: second_frame}, internal_state) do
+    {Cpp.join_frames(first_frame, second_frame, internal_state), internal_state}
+  end
+end
+
+defmodule Membrane.VideoCompositor.OpenGL.Rust do
+  @moduledoc """
+  This module implements video composition in OpenGL with a Rust backend
+  """
+
+  @behaviour Membrane.VideoCompositor.FrameCompositor
+
+  alias Membrane.VideoCompositor.OpenGL.Native.Rust
+
+  @impl true
+  def init(%Membrane.RawVideo{width: width, height: height, pixel_format: pixel_format}) do
+    input_caps = %Rust.RawVideo{width: width, height: height, pixel_format: pixel_format}
+    output_caps = %Rust.RawVideo{width: width, height: 2 * height, pixel_format: pixel_format}
+
+    Rust.init(input_caps, input_caps, output_caps)
+  end
+
+  @impl true
+  def merge_frames(%{first: first, second: second}, internal_state) do
+    {Rust.join_frames(first, second, internal_state), internal_state}
+  end
+end
