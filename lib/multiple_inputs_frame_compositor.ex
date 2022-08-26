5a8fd16d5760e87570ff7f846e7168bf6d22e06e
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-08-26 11:55:16 +0200

    Create multiple inputs behaviour

---
+++ lib/multiple_inputs_frame_compositor.ex
@@ -0,0 +1,26 @@
+defmodule Membrane.VideoCompositor.MultipleInputsFrameCompositor do
+  @moduledoc """
+  This module defines behaviour for different frame composition implementations supporting multiple input frames.
+  """
+  alias Membrane.RawVideo
+
+  @callback init(output_caps :: RawVideo.t()) :: {:ok, state :: any()}
+
+  @callback merge_frames(
+              frames :: [binary()],
+              internal_state :: any()
+            ) :: {{:ok, merged_frames :: binary()}, state :: any()}
+
+  @callback add_video(
+              input_caps :: RawVideo.t(),
+              position :: {x :: non_neg_integer(), y :: non_neg_integer()},
+              id :: non_neg_integer()
+            ) :: {:ok, state :: any()}
+
+  @callback remove_video(id :: non_neg_integer()) :: {:ok, state :: any()}
+
+  @callback set_position(
+              position :: {x :: non_neg_integer(), y :: non_neg_integer()},
+              id :: non_neg_integer()
+            ) :: {:ok, state :: any()}
+end
