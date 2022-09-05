339eb0c999297aff6bfed8d504401f502b2148de
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2022-09-05 10:23:40 +0200

    Add a basic skeleton for the `wgpu`-based video compositor.

---
+++ lib/video_compositor/wgpu_native.ex
@@ -0,0 +1,16 @@
+defmodule Membrane.VideoCompositor.Wgpu.Native do
+  @moduledoc false
+  use Rustler,
+    otp_app: :membrane_video_compositor_plugin,
+    crate: "membrane_videocompositor_wgpu"
+
+  alias Membrane.VideoCompositor.OpenGL.Native.Rust.RawVideo
+
+  @spec init(RawVideo.t(), RawVideo.t(), RawVideo.t()) :: {:ok, any()} | {:error, any}
+  def init(_first_video, _second_video, _out_video), do: error()
+
+  @spec join_frames(any(), binary(), binary()) :: {:ok, binary()} | {:error, any()}
+  def join_frames(_state, _upper, _lower), do: error()
+
+  defp error(), do: :erlang.nif_error(:nif_not_loaded)
+end
