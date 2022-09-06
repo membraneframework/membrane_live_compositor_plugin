b4eecd480055d594f263c5be1ad06ecaf0a25998
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-09-06 16:29:19 +0200

    Add testing pipelines for multiple inputs
    Extend `Pipeline` sublib and implement `H264` pipeline

---
+++ test/support/multiple_inputs/pipeline_h264_video.ex
@@ -0,0 +1,45 @@
+defmodule Membrane.VideoCompositor.Test.Pipeline.H264.MultipleInputs do
+  @moduledoc """
+  Pipeline for testing composing of many videos.
+  """
+
+  use Membrane.Pipeline
+
+  @doc """
+  handle_init(%{
+      paths: %{
+        first_video_path: String.t(),
+        second_video_path: String.t(),
+        output_path: String.t()
+      },
+      caps: RawVideo,
+      implementation: :ffmpeg | :opengl | :nx
+  })
+  """
+  @impl true
+  def handle_init(options) do
+    decoder = Membrane.VideoCompositor.Test.Pipeline.H264.TwoInputsParser
+    encoder = Membrane.H264.FFmpeg.Encoder
+
+    options = Map.put(options, :decoder, decoder)
+    options = Map.put_new(options, :encoder, encoder)
+
+    options =
+      Map.put_new(options, :compositor, %Membrane.VideoCompositor.MultipleInputs.VideoCompositor{
+        implementation: options.implementation,
+        caps: options.caps
+      })
+
+    Membrane.VideoCompositor.Pipeline.ComposeMultipleInputs.handle_init(options)
+  end
+
+  @impl true
+  def handle_element_end_of_stream({:file_sink, :input}, _context, state) do
+    {{:ok, [playback: :terminating]}, state}
+  end
+
+  @impl true
+  def handle_element_end_of_stream(_pad, _context, state) do
+    {:ok, state}
+  end
+end
