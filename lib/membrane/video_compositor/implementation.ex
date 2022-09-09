a3b6ef3ec89ee7c798e7a6568bc315fb86e99311
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-09-09 11:46:11 +0200

    Remove old VC, move new to corresponding dirs
    Modules names are still outdated

--- lib/membrane/video_compositor/implementation.ex
+++ lib/membrane/video_compositor/implementation.ex
@@ -1,6 +1,6 @@
-defmodule Membrane.VideoCompositor.Implementations do
+defmodule Membrane.VideoCompositor.MultipleInputs.VideoCompositor.Implementations do
   @moduledoc """
-  A module describing video compositor implementation type and implementing
+  A module describing multiple input video compositor implementation type and implementing
   functions related with implementation format.
   """
 
@@ -11,35 +11,30 @@ defmodule Membrane.VideoCompositor.Implementations do
   def get_implementation_module(implementation) do
     case implementation do
       :ffmpeg ->
-        {:ok, Membrane.VideoCompositor.Implementations.FFmpeg}
+        raise ":ffmpeg is not implemented yet"
 
       :opengl_cpp ->
-        {:ok, Membrane.VideoCompositor.Implementations.OpenGL.Cpp}
+        raise ":opengl_cpp is not implemented yet"
 
       :opengl_rust ->
-        {:ok, Membrane.VideoCompositor.Implementations.OpenGL.Rust}
+        {:ok, Membrane.VideoCompositor.MultipleInputs.OpenGL.Rust}
 
       :nx ->
-        {:ok, Membrane.VideoCompositor.Nx}
+        raise ":nx is not implemented yet"
 
       _other ->
         {:error, "Format not supported"}
     end
   end
 
-  @spec get_test_implementations() :: list()
-  def get_test_implementations() do
-    test_implementations = [:ffmpeg, :nx]
-    test_implementations
-  end
-
-  @spec get_all_implementations() :: list()
+  @spec get_all_implementations() :: list(implementation_t)
   def get_all_implementations() do
-    [:ffmpeg, :opengl_cpp, :opengl_rust, :nx]
+    [:opengl_rust]
   end
 
   @spec get_implementation_atom_from_string(String.t()) :: implementation_t()
-  def get_implementation_atom_from_string(implementation_string) do
+  def get_implementation_atom_from_string(implementation_string)
+      when is_binary(implementation_string) do
     case implementation_string do
       "ffmpeg" -> :ffmpeg
       "opengl_cpp" -> :opengl_cpp
--- lib/membrane/video_compositor/implementation/opengl/rust.ex
+++ lib/membrane/video_compositor/implementation/opengl/rust.ex
@@ -1,27 +1,37 @@
-defmodule Membrane.VideoCompositor.Implementations.OpenGL.Rust do
+defmodule Membrane.VideoCompositor.MultipleInputs.OpenGL.Rust do
   @moduledoc """
   This module implements video composition in OpenGL with a Rust backend
   """
-
-  @behaviour Membrane.VideoCompositor.FrameCompositor
+  @behaviour Membrane.VideoCompositor.MultipleInputs.VideoCompositor.FrameCompositor
 
   alias Membrane.VideoCompositor.Implementations.OpenGL.Native.Rust
 
   @impl true
-  def init(%Membrane.RawVideo{width: width, height: height, pixel_format: pixel_format}) do
-    input_caps = %Rust.RawVideo{width: width, height: height, pixel_format: pixel_format}
-    output_caps = %Rust.RawVideo{input_caps | height: 2 * height}
-
-    {:ok, internal_state} = Rust.init(output_caps)
-
-    Rust.add_video(internal_state, 0, input_caps, %Rust.Position{x: 0, y: 0})
-    Rust.add_video(internal_state, 1, input_caps, %Rust.Position{x: 0, y: input_caps.height})
-
-    {:ok, internal_state}
+  def init(output_caps) do
+    {:ok, output_caps} = Rust.RawVideo.from_membrane_raw_video(output_caps)
+    Rust.init(output_caps)
   end
 
   @impl true
-  def merge_frames(%{first: first, second: second}, internal_state) do
-    {Rust.join_frames(internal_state, [{0, first}, {1, second}]), internal_state}
+  def add_video(id, input_caps, position, internal_state) do
+    {:ok, input_caps} = Rust.RawVideo.from_membrane_raw_video(input_caps)
+    {:ok, position} = Rust.Position.from_tuple(position)
+    {Rust.add_video(internal_state, id, input_caps, position), internal_state}
+  end
+
+  @impl true
+  def remove_video(id, internal_state) do
+    {Rust.remove_video(internal_state, id), internal_state}
+  end
+
+  @impl true
+  def merge_frames(frames, internal_state) do
+    {Rust.join_frames(internal_state, frames), internal_state}
+  end
+
+  @impl true
+  def set_position(id, position, internal_state) do
+    {:ok, position} = Rust.Position.from_tuple(position)
+    {Rust.set_position(internal_state, id, position), internal_state}
   end
 end
