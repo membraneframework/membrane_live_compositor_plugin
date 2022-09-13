8d9d1ea9f07347fb2167bbdd81b5fc98a396db1d
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-09-13 16:27:10 +0200

    Remove unsupported implementations atoms

--- lib/membrane/video_compositor/implementations.ex
+++ lib/membrane/video_compositor/implementations.ex
@@ -5,22 +5,16 @@ defmodule Membrane.VideoCompositor.Implementations do
   """
 
   @typedoc "Define video compositor implementation types"
-  @type implementation_t :: :ffmpeg | :opengl_cpp | :opengl_rust | :nx
+  @type implementation_t :: :wgpu | :opengl_rust
 
   @spec get_implementation_module(implementation_t) :: {:ok, module()} | {:error, String.t()}
   def get_implementation_module(implementation) do
     case implementation do
-      :ffmpeg ->
-        raise ":ffmpeg is not implemented yet"
-
-      :opengl_cpp ->
-        raise ":opengl_cpp is not implemented yet"
+      :wgpu ->
+        raise ":wgpu is not implemented yet"
 
       :opengl_rust ->
         {:ok, Membrane.VideoCompositor.OpenGL.Rust}
-
-      :nx ->
-        raise ":nx is not implemented yet"
 
       _other ->
         {:error, "Format not supported"}
@@ -36,10 +30,8 @@ defmodule Membrane.VideoCompositor.Implementations do
   def get_implementation_atom_from_string(implementation_string)
       when is_binary(implementation_string) do
     case implementation_string do
-      "ffmpeg" -> :ffmpeg
-      "opengl_cpp" -> :opengl_cpp
       "opengl_rust" -> :opengl_rust
-      "nx" -> :nx
+      "wgpu" -> :wgpu
     end
   end
 end
