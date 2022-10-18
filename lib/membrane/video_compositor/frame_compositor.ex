dd85a52052ddd4dd3b42ad4731481f45006d03bc
jerzywilczek <72213407+jerzywilczek@users.noreply.github.com>
2022-10-18 10:49:52 +0200

    Reformat docs
    Co-authored-by: DominikWolek <dominik.wolek@swmansion.com>

--- lib/membrane/video_compositor/frame_compositor.ex
+++ lib/membrane/video_compositor/frame_compositor.ex
@@ -13,6 +13,7 @@ defmodule Membrane.VideoCompositor.FrameCompositor do
 
   @doc """
   Uploads a frame to the compositor.
+
   If all videos have provided input frames with a current enough pts, this will also render and return a composed frame.
   """
   @callback upload_frame(internal_state_t(), id_t(), frame_with_pts()) ::
@@ -26,6 +27,7 @@ defmodule Membrane.VideoCompositor.FrameCompositor do
 
   @doc """
   Registers a new input video with the given numerical `id`.
+
   Provided `id` should be unique within all previous ones, otherwise the compositor may or may not replace
   the old video with this id with a new one.
   `x` and `y` are pixel coordinates specifying where the top-right corner of the video should be.
@@ -51,6 +53,7 @@ defmodule Membrane.VideoCompositor.FrameCompositor do
 
   @doc """
   Video of the given `id` should be registered, using `id` of nonexistent video may panic the compositor.
+
   `x` and `y` are pixel coordinates specifying where the top-right corner of the video should be.
   `z` must be a float between 0.0 and 1.0, and it determines which videos are drawn in front of others.
   A video with a lower `z` coordinate will cover videos with higher `z` coordinates.
