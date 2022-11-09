b1df3ab3ffba5c769218c4667cd06d1160bce583
Wojciech Barczyński <104033489+WojciechBarczynski@users.noreply.github.com>
2022-11-09 20:28:51 +0100

    Add video scale and z value option for elixir VC (#59)

--- pipeline/lib/membrane/video_compositor/pipeline/utility/input_stream.ex
+++ pipeline/lib/membrane/video_compositor/pipeline/utility/input_stream.ex
@@ -5,8 +5,10 @@ defmodule Membrane.VideoCompositor.Pipeline.Utility.InputStream do
   @type t() :: %__MODULE__{
           caps: Membrane.RawVideo.t(),
           position: {non_neg_integer(), non_neg_integer()},
+          z_value: float(),
+          scale: float(),
           input: String.t() | Membrane.Source
         }
-  @enforce_keys [:caps, :position, :input]
-  defstruct caps: nil, position: nil, input: nil
+  @enforce_keys [:caps, :position, :scale, :input]
+  defstruct caps: nil, z_value: nil, position: nil, scale: nil, input: nil
 end
