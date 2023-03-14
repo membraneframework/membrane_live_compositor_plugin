56e38980b5dbf78445150204ad708d2aae6681ac
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-03-14 09:47:29 +0100

    Describe kinds of objects in Scene moduledoc

--- lib/membrane/video_compositor/scene.ex
+++ lib/membrane/video_compositor/scene.ex
@@ -2,6 +2,13 @@ defmodule Membrane.VideoCompositor.Scene do
   @moduledoc """
   Structure representing a top level specification of what is Video Compositor
   supposed to render.
+
+  The main part of the Scene are `Membrane.VideoCompositor.Scene.Object`s
+  and interactions between them. There are two kinds of Objects:
+  - `Membrane.VideoCompositor.Scene.Object.Texture` - single input object,
+  taking frames and applying a series of transformations onto it.
+  - `Membrane.VideoCompositor.Scene.Object.Layout` - combining
+  frames from multiple inputs into a single output.
   """
 
   alias Membrane.VideoCompositor.Scene.Object
