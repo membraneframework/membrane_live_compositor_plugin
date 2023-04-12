14c6a061e830727ab7f44043f585506277790d4f
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-04-12 17:33:00 +0200

    Add initial version of callbacks

---
+++ lib/membrane/video_compositor/callbacks.ex
@@ -0,0 +1,76 @@
+defmodule Membrane.VideoCompositor.Callbacks do
+  alias __MODULE__.Context
+  alias Membrane.{Pad, StreamFormat, Time}
+  alias Membrane.VideoCompositor.Scene
+
+  @type state :: any()
+
+  @type video_details :: {pad :: Pad.ref_t(), format :: StreamFormat.t()}
+
+  @type callback_return :: {scene :: Scene.t(), state :: state()}
+
+  @callback handle_init(ctx :: Context.Init.t()) :: callback_return()
+
+  @callback handle_video_add(
+              video :: video_details(),
+              ctx :: Context.VideoAdd.t(),
+              state :: state()
+            ) :: callback_return()
+
+  @callback handle_video_remove(
+              video :: video_details(),
+              ctx :: Context.VideoRemove.t(),
+              state :: state()
+            ) :: callback_return()
+
+  @callback handle_info(msg :: any(), ctx :: Context.Info.t(), state :: state()) ::
+              callback_return() | {start_ts :: Time.t(), callback_return()}
+end
+
+defmodule Membrane.VideoCompositor.Callbacks.Context do
+  alias Membrane.VideoCompositor.Scene
+
+  @enforce_keys [:input_pads, :scenes_queue, :current_scene]
+  defstruct @enforce_keys
+
+  @type intput_pads :: list(Membrane.Pad.ref_t())
+
+  @type start_scene_timestamp :: Membrane.Time.t()
+  @type scenes_queue :: list({Scene.t(), start_scene_timestamp()})
+
+  @type t :: %__MODULE__{
+          input_pads: intput_pads(),
+          scenes_queue: scenes_queue(),
+          current_scene: Scene.t()
+        }
+end
+
+defmodule Membrane.VideoCompositor.Callbacks.Context.Init do
+  alias Membrane.VideoCompositor.Callbacks.Context
+  @type t :: Context.t()
+end
+
+defmodule Membrane.VideoCompositor.Callbacks.Context.VideoAdd do
+  alias Membrane.VideoCompositor.Callbacks.Context
+  @type t :: Context.t()
+end
+
+defmodule Membrane.VideoCompositor.Callbacks.Context.VideoRemove do
+  alias Membrane.VideoCompositor.Callbacks.Context
+  @type t :: Context.t()
+end
+
+defmodule Membrane.VideoCompositor.Callbacks.Context.Info do
+  alias Membrane.Time
+  alias Membrane.VideoCompositor.Callbacks.Context
+
+  @enforce_keys [:input_pads, :scenes_queue, :current_scene, :earliest_start]
+  defstruct @enforce_keys
+
+  @type t :: %__MODULE__{
+          input_pads: Context.intput_pads(),
+          scenes_queue: Context.scenes_queue(),
+          current_scene: Scene.t(),
+          earliest_start: Time.t()
+        }
+end
