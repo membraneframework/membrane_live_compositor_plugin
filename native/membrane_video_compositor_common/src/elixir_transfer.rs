c1b5a05900c96a65cad9ab071cb54c92cf02f077
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2023-05-10 13:31:52 +0200

    Reimplement elixir trait packets with a macro.

--- native/membrane_video_compositor_common/src/elixir_transfer.rs
+++ native/membrane_video_compositor_common/src/elixir_transfer.rs
@@ -39,70 +39,45 @@ impl rustler::Encoder for ElixirTypePacket {
     }
 }
 
-pub struct ElixirTransformationPacket {
-    pointer: (usize, usize),
+macro_rules! trait_packet {
+    ($trait:ident, $struct:ident) => {
+        pub struct $struct {
+            pointer: (usize, usize),
+        }
+
+        impl $struct {
+            pub fn encode(payload: impl $trait) -> Self {
+                let payload: ::std::sync::Arc<dyn $trait> = ::std::sync::Arc::new(payload);
+                let pointer: (usize, usize) = unsafe { ::std::mem::transmute(payload) };
+                Self { pointer }
+            }
+
+            /// # Safety
+            /// The caller must ensure that all of the following conditions are met:
+            ///  - The packet must have been encoded in the same address space as the call site
+            ///  - This method can only be called **once** per packet's content, i.e. You cannot copy
+            ///    this struct in elixir and pass it to two separate rust functions that would decode it twice
+            ///  - The call site must have been compiled with the same compiler as the encoding site
+            pub unsafe fn decode(self) -> Arc<dyn $trait> {
+                unsafe { ::std::mem::transmute(self.pointer) }
+            }
+        }
+
+        impl<'a> ::rustler::Decoder<'a> for $struct {
+            fn decode(term: rustler::Term<'a>) -> rustler::NifResult<Self> {
+                let pointer = rustler::Decoder::decode(term)?;
+                Ok(Self { pointer })
+            }
+        }
+
+        impl ::rustler::Encoder for $struct {
+            fn encode<'a>(&self, env: rustler::Env<'a>) -> rustler::Term<'a> {
+                self.pointer.encode(env)
+            }
+        }
+    };
 }
 
-impl ElixirTransformationPacket {
-    pub fn encode(payload: impl UntypedTransformation) -> Self {
-        let payload: Arc<dyn UntypedTransformation> = Arc::new(payload);
-        let pointer: (usize, usize) = unsafe { std::mem::transmute(payload) };
-        Self { pointer }
-    }
+trait_packet!(UntypedTransformation, ElixirTransformationPacket);
 
-    /// # Safety
-    /// The caller must ensure that all of the following conditions are met:
-    ///  - The packet must have been encoded in the same address space as the call site
-    ///  - The packet must not have been cloned or duplicated in any way in elixir
-    ///  - The call site must have been compiled with the same compiler as the encoding site
-    pub unsafe fn decode(self) -> Arc<dyn UntypedTransformation> {
-        unsafe { std::mem::transmute(self.pointer) }
-    }
-}
-
-impl<'a> rustler::Decoder<'a> for ElixirTransformationPacket {
-    fn decode(term: rustler::Term<'a>) -> rustler::NifResult<Self> {
-        let pointer = rustler::Decoder::decode(term)?;
-        Ok(Self { pointer })
-    }
-}
-
-impl rustler::Encoder for ElixirTransformationPacket {
-    fn encode<'a>(&self, env: rustler::Env<'a>) -> rustler::Term<'a> {
-        self.pointer.encode(env)
-    }
-}
-
-pub struct ElixirLayoutPacket {
-    pointer: (usize, usize),
-}
-
-impl ElixirLayoutPacket {
-    pub fn encode(payload: impl UntypedLayout) -> Self {
-        let payload: Arc<dyn UntypedLayout> = Arc::new(payload);
-        let pointer: (usize, usize) = unsafe { std::mem::transmute(payload) };
-        Self { pointer }
-    }
-
-    /// # Safety
-    /// The caller must ensure that all of the following conditions are met:
-    ///  - The packet must have been encoded in the same address space as the call site
-    ///  - The packet must not have been cloned or duplicated in any way in elixir
-    ///  - The call site must have been compiled with the same compiler as the encoding site
-    pub unsafe fn decode(self) -> Arc<dyn UntypedLayout> {
-        unsafe { std::mem::transmute(self.pointer) }
-    }
-}
-
-impl<'a> rustler::Decoder<'a> for ElixirLayoutPacket {
-    fn decode(term: rustler::Term<'a>) -> rustler::NifResult<Self> {
-        let pointer = rustler::Decoder::decode(term)?;
-        Ok(Self { pointer })
-    }
-}
-
-impl rustler::Encoder for ElixirLayoutPacket {
-    fn encode<'a>(&self, env: rustler::Env<'a>) -> rustler::Term<'a> {
-        self.pointer.encode(env)
-    }
-}
+trait_packet!(UntypedLayout, ElixirLayoutPacket);
