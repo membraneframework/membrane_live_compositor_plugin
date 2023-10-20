8e11506b98f5d824d5ced43c062def89fc5351dd
Wojciech Barczynski <barczynski.w@gmail.com>
2023-10-20 08:23:40 +0200

    Remove samples from git

--- examples/layout_with_shader.exs
+++ examples/layout_with_shader.exs
@@ -1,5 +1,9 @@
 alias Membrane.VideoCompositor.Examples.LayoutWithShader.Pipeline
 
+unless File.exists?("samples/testsrc.h264") do
+  {_, 0} = System.cmd("ffmpeg", ["-f", "lavfi", "-i" "testsrc=duration=30:size=1280x720:rate=30", "-pix_fmt", "yuv420p", "testsrc.h264"])
+end
+
 {:ok, _supervisor, _pid} = Pipeline.start_link(%{})
 
 :timer.sleep(100_000)
--- examples/transition.exs
+++ examples/transition.exs
@@ -1,5 +1,19 @@
 alias Membrane.VideoCompositor.Examples.Transition.Pipeline
 
+unless File.exists?("samples/testsrc.h264") do
+  {_, 0} =
+    System.cmd("ffmpeg", [
+      "-y",
+      "-f",
+      "lavfi",
+      "-i",
+      "testsrc=duration=30:size=1280x720:rate=30",
+      "-pix_fmt",
+      "yuv420p",
+      "samples/testsrc.h264"
+    ])
+end
+
 {:ok, _supervisor, _pid} = Pipeline.start_link(%{})
 
 :timer.sleep(100_000)
