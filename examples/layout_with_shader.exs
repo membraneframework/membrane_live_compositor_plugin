2312bf97c98bf64f12bb750d71a66b67eb89fd18
Wojciech Barczynski <barczynski.w@gmail.com>
2023-10-20 08:26:34 +0200

    fmt

--- examples/layout_with_shader.exs
+++ examples/layout_with_shader.exs
@@ -3,7 +3,17 @@ alias Membrane.VideoCompositor.Examples.LayoutWithShader.Pipeline
 File.mkdir_p!("samples")
 
 unless File.exists?("samples/testsrc.h264") do
-  {_, 0} = System.cmd("ffmpeg", ["-f", "lavfi", "-i" "testsrc=duration=30:size=1280x720:rate=30", "-pix_fmt", "yuv420p", "testsrc.h264"])
+  {_, 0} =
+    System.cmd("ffmpeg", [
+      "-y",
+      "-f",
+      "lavfi",
+      "-i",
+      "testsrc=duration=30:size=1280x720:rate=30",
+      "-pix_fmt",
+      "yuv420p",
+      "samples/testsrc.h264"
+    ])
 end
 
 {:ok, _supervisor, _pid} = Pipeline.start_link(%{})
