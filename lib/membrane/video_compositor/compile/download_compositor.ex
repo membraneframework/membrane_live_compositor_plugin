a9adf24f46dc9172bf54e348a7e39e2da8966863
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2024-04-08 11:51:57 +0200

    Update binary to 0.2.0-rc.4

--- lib/membrane/video_compositor/compile/download_compositor.ex
+++ lib/membrane/video_compositor/compile/download_compositor.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.Compile.DownloadCompositor do
   use Mix.Task
   require Membrane.Logger
 
-  @lc_version "v0.2.0-rc.2"
+  @lc_version "v0.2.0-rc.4"
 
   @impl Mix.Task
   def run(_args) do
