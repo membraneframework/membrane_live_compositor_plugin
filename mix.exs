b0eb5ec85f8d7c7077af3da59aa7802b7dc1d986
Wojciech Barczyński <104033489+WojciechBarczynski@users.noreply.github.com>
2024-07-04 12:30:42 +0200

    Release v0.9.1 (#198)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.LiveCompositor.Mixfile do
   use Mix.Project
 
-  @version "0.9.0"
+  @version "0.9.1"
   @github_url "https://github.com/membraneframework/membrane_live_compositor_plugin"
 
   def project do
