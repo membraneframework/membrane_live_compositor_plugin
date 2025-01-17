303a2c940548b23564ead44919b5aa069033753c
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-01-17 15:22:24 +0100

    Fix credo

--- lib/membrane/live_compositor/server_runner.ex
+++ lib/membrane/live_compositor/server_runner.ex
@@ -85,7 +85,8 @@ defmodule Membrane.LiveCompositor.ServerRunner do
   end
 
   defp get_host_by_name!(hostname) do
-    with {:ok, {:hostent, _, _, :inet, 4, ips}} <- :inet.gethostbyname(hostname, :inet),
+    with {:ok, {:hostent, _hostname, _aliases, :inet, 4, ips}} <-
+           :inet.gethostbyname(hostname, :inet),
          ipv4 when ipv4 != nil <- Enum.find(ips, &:inet.is_ipv4_address/1) do
       ipv4
     else
